60e7a62c07626691fa1633fe847f3923b8eab1fa
Alex Luecke <me@alexjl.com>
2015-04-14 20:43:33 -0700

    init

---
+++ main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let rna_string = "".to_string();
+    for j in (0..n) {
+        let mut i = j-1;
+        while i > 0 {
+            i -= 1;
+        }
+    }
+}
+
+fn beta(x: isize, y: isize, S: &str) -> usize {
+    0
+}
